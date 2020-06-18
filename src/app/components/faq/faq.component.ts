e96a2f05a2a1940c7711f443463c8cb626cf4419
Panbaba9n <Goldgraalar@gmail.com>
2020-06-18 11:21:19 +0300

    editting FAQ section

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,4 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { IFAQ } from '@models/faq-question.interface';
+
+const QUESTIONS: IFAQ[] = [
+  {
+    question: 'Where is Tend available?',
+    answer: 'We operate in all of Melbourne.',
+    isExpanded: true
+  },
+  {
+    question: 'Is Tend’s offering a “self-management” service?',
+    answer: 'Tend’s service is full care management service which is different from self-management. Under self management you are responsible for sourcing and scheduling your in-home services. Tend will introduce you to the best in home services, set up those services and provide on-going scheduling.',
+    isExpanded: false
+  },
+  {
+    question: 'Does Tend also charge for Package Administration?',
+    answer: 'Many providers will break it down into two different costs - package management and care management. But we think it is easier for you if we give you one price for both the care management and administration. So the cost you see in our Pricing Plan is the total cost you will pay.',
+    isExpanded: false
+  },
+  {
+    question: 'Does Tend charge any Basic Daily Fee, Establishment Fee or Exit Fee?',
+    answer: 'No. At Tend, you just pay for your Care Management.',
+    isExpanded: false
+  },
+  {
+    question: 'Does Tend charge for purchases of goods?',
+    answer: 'No. Unlike most providers who charge up to 15% on your purchases, Tend charges zero.',
+    isExpanded: false
+  }
+];
 
 @Component({
   selector: 'app-faq',
@@ -6,34 +35,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
-  questions: any[];
+  questions: IFAQ[];
 
-  constructor() {
-    this.questions = [
-      {
-        question: 'Where is Tend available?',
-        answer: 'We operate in all of Melbourne.',
-        isExpanded: true
-      },
-      {
-        question: 'Is Tend’s offering a “self-management” service?',
-        answer: 'Tend’s service is full care management service which is different from self-management. Under self management you are responsible for sourcing and scheduling your in-home services. Tend will introduce you to the best in home services, set up those services and provide on-going scheduling.',
-        isExpanded: false
-      },
-      {
-        question: 'Does Tend also charge for Package Administration?',
-        answer: 'Many providers will break it down into two different costs - package management and care management. But we think it is easier for you if we give you one price for both the care management and administration. So the cost you see in our Pricing Plan is the total cost you will pay.',
-        isExpanded: false
-      },
-      {
-        question: 'Does Tend charge any Basic Daily Fee, Establishment Fee or Exit Fee?',
-        answer: 'No, we do not. The only charge we have is a 10% handling fee for purchases of goods (capped at $100).',
-        isExpanded: false
-      }
-    ];
-  }
+  constructor() { }
 
   ngOnInit(): void {
+    this.questions = QUESTIONS;
   }
 
   toggleExpand(questionClicked) {
