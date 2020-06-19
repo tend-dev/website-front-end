ba834517dcd9aa0de89de0e8128030fa4c747ddb
Panbaba9n <Goldgraalar@gmail.com>
2020-06-19 11:10:31 +0300

    re-develop top menu

--- src/app/components/mobile-menu/mobile-menu.component.ts
+++ src/app/components/mobile-menu/mobile-menu.component.ts
@@ -1,27 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 import { ContactFormComponent } from '../contact-form/contact-form.component';
 import { ContactService } from '@app/services/contact.service';
+import { MenuLinksService } from '@services/menu-links/menu-links.service';
+import { MenuLink } from '@models/menu-link.interface';
 
 @Component({
   selector: 'app-mobile-menu',
   templateUrl: './mobile-menu.component.html',
   styleUrls: ['./mobile-menu.component.scss']
 })
-export class MobileMenuComponent implements OnInit {
+export class MobileMenuComponent implements OnDestroy {
+  menuLinks$: Observable<MenuLink[]> = this.menuLinksService.links$;
+  private routePath: string[];
+  private fragment: string;
+  private linkSub: Subscription;
 
-  constructor(public dialogRef: MatDialogRef<ContactFormComponent>, public contactService: ContactService) {
+  constructor(
+    private dialogRef: MatDialogRef<ContactFormComponent>,
+    private contactService: ContactService,
+    private menuLinksService: MenuLinksService,
+    private router: Router
+  ) {
+    this.subscribeToClose()
+  }
+
+  ngOnDestroy() {
+    this.linkSub.unsubscribe();
+  }
+
+  subscribeToClose() {
+    this.linkSub = this.dialogRef.afterClosed().subscribe(() => {
+      this.router.navigate(this.routePath, { fragment: this.fragment });
+      this.routePath = null;
+      this.fragment = null;
+    })
   }
 
   openDialog(): void {
     this.contactService.openDialog();
   }
 
-  ngOnInit(): void {
-  }
-
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  routeTo({routePath, fragment}: { routePath: string[], fragment: string }) {
+    this.routePath = routePath;
+    this.fragment = fragment;
   }
 }
