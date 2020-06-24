d77a0ddf11a3e4eed9a0abef552c4f1100222262
Panbaba9n <Goldgraalar@gmail.com>
2020-06-24 12:38:14 +0300

    footer / get in touch section

--- src/app/components/get-in-touch/get-in-touch.module.ts
+++ src/app/components/get-in-touch/get-in-touch.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GetInTouchComponent } from './get-in-touch.component';
+import { RouterModule } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
-
 
 
 @NgModule({
   declarations: [GetInTouchComponent],
   imports: [
     CommonModule,
+    RouterModule,
     MatButtonModule
   ],
   exports: [GetInTouchComponent]
