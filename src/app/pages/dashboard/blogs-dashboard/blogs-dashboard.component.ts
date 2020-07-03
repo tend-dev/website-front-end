38e2b8bdb067fb8d15a7556602304f772c493f53
Panbaba9n <Goldgraalar@gmail.com>
2020-07-03 12:40:06 +0300

    blog-dashboards page

--- src/app/pages/dashboard/blogs-dashboard/blogs-dashboard.component.ts
+++ src/app/pages/dashboard/blogs-dashboard/blogs-dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
+import { EditBlogState, editBlog } from '@store/blog-state'
 import { BlogsService } from '@services/blogs.service';
+import { Blog } from '@models/blog.interface';
 
 @Component({
   selector: 'app-blogs-dashboard',
@@ -8,12 +12,35 @@ import { BlogsService } from '@services/blogs.service';
   styleUrls: ['./blogs-dashboard.component.scss']
 })
 export class BlogsDashboardComponent implements OnInit {
+  // public blogs$: Observable<Blog[]> = this.blogsService.blogs$;
+  public blogs$: Observable<Blog[]>;
+  public loading$: Observable<boolean>;
 
   constructor(
-    private blogsService: BlogsService
+    private blogsService: BlogsService,
+    private store: Store<EditBlogState>
   ) { }
 
   ngOnInit(): void {
+    this.blogs$ = this.blogsService.entities$;
+    this.loading$ = this.blogsService.loading$;
+
+    this.getBlogs();
   }
 
+  getBlogs() {
+    this.blogsService.getBlogs();
+  }
+
+  delete(blog: Blog) {
+    this.blogsService.delete(blog.id);
+  }
+
+  edit(blog: Blog) {
+    this.store.dispatch(editBlog(blog));
+  }
+
+  trackByArticles(index: number, blog: Blog): string {
+    return blog.id;
+  }
 }
--- src/app/pages/dashboard/blogs-dashboard/blogs-dashboard.module.ts
+++ src/app/pages/dashboard/blogs-dashboard/blogs-dashboard.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { BlogsDashboardRoutingModule } from './blogs-dashboard-routing.module';
 import { BlogsDashboardComponent } from './blogs-dashboard.component';
+import { BlogFormModule } from '../components/blog-form/blog-form.module';
 
 
 @NgModule({
   declarations: [BlogsDashboardComponent],
   imports: [
     CommonModule,
-    BlogsDashboardRoutingModule
+    BlogsDashboardRoutingModule,
+
+    BlogFormModule
   ]
 })
 export class BlogsDashboardModule { }
