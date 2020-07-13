a1fa209a001a75081197d748effe0894a97e2dad
Panbaba9n <Goldgraalar@gmail.com>
2020-07-13 12:35:21 +0300

    add logout btn

--- src/app/pages/authorization/authorization/authorization.component.ts
+++ src/app/pages/authorization/authorization/authorization.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
 import { AuthorizationService } from '../shared/providers/authorization.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html',
   styleUrls: ['./authorization.component.scss']
 })
-export class AuthorizationComponent implements OnInit {
+export class AuthorizationComponent implements OnInit, OnDestroy {
   public form: FormGroup;
   public error: string;
+  private authSub: Subscription;
+  public isAuthenticated: boolean;
 
   get email(): AbstractControl { return this.form.get('email'); }
 
@@ -22,9 +25,15 @@ export class AuthorizationComponent implements OnInit {
 
   ngOnInit() {
     this.buildForm();
+
+    this.refreshAuthstatus();
   }
 
-  public onSubmit(): void {
+  ngOnDestroy() {
+    this.authSub.unsubscribe();
+  }
+
+  onSubmit(): void {
     if (!this.form.valid) {
       return;
     }
@@ -37,6 +46,18 @@ export class AuthorizationComponent implements OnInit {
           this.error = 'Email or password is wrong';
         }
       );
+  }
+
+  logOut() {
+    this.authorizationService.signOut();
+    this.refreshAuthstatus();
+  }
+
+  // TODO: implement via STORE or service as observable
+  private refreshAuthstatus() {
+    this.authSub = this.authorizationService.isAuthenticated().subscribe(res => {
+      this.isAuthenticated = res;
+    });
   }
 
   private buildForm(): void {
