e819635704e7eaf4c9bbeee9f61ac904d451924a
Panbaba9n <Goldgraalar@gmail.com>
2020-07-13 13:49:31 +0300

    https to http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://159.65.47.27:3000/api',
-  contactApiURL: 'https://www.tend.care:446/api/',
-  backEndURL: 'https://159.65.47.27:3000'
+  apiURL: 'http://159.65.47.27:3000/api',
+  contactApiURL: 'http://www.tend.care:446/api/',
+  backEndURL: 'http://159.65.47.27:3000'
 };
