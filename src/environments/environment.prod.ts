cd8f01e8d57011452ea38958c9f77dab1b07283f
Panbaba9n <Goldgraalar@gmail.com>
2020-08-04 13:27:36 +0300

    set https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://tend.care:3000/api', // 'http://159.65.47.27:3000/api'
-  contactApiURL: 'http://www.tend.care:446/api/',
-  backEndURL: 'http://tend.care:3000' // 'http://159.65.47.27:3000'
+  apiURL: 'https://tend.care:3000/api', // 'http://159.65.47.27:3000/api'
+  contactApiURL: 'https://www.tend.care:446/api/',
+  backEndURL: 'https://tend.care:3000' // 'http://159.65.47.27:3000'
 };
